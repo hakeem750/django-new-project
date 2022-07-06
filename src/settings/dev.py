7b5fb5acfbdb7fc1eb5cb44dd9c467872c42eea9
myrage <hakeemy056@gmail.com>
2022-07-06 17:32:43 +0100

    new commit

--- src/settings/dev.py
+++ src/settings/dev.py
@@ -1,9 +1,9 @@
 from .base import *
 from decouple import config
 
-
-
-SECRET_KEY = config("SECRET_KEY")
+# Delete this placeholder and uncomment the comfig secret key for a reliable secret key
+SECRET_KEY = "django-app-place-holder"
+# SECRET_KEY = config("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
